175ea755eba93478e6206484da3b1f0f74a08f89
nakamichi-sifi <tomohironakamichi@sifi.co.jp>
2025-07-22 14:12:02 +0900

    バージョン番号表示画面 追加

--- src/app.py
+++ src/app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.write("""
-# My first app
+APP_NAME = "My first app"
+APP_VERSION = "0.3.0"
+
+st.set_page_config(
+    page_title=APP_NAME,
+    page_icon="👋",
+    menu_items={
+        'about': f"{APP_NAME} v{APP_VERSION}"
+    }
+)
+
+st.write(f"""
+# {APP_NAME}
 Hello *world!*
 """)
